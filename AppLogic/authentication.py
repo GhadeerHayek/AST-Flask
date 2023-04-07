2c089ebcc07ad737ded4df477761c1e1a765db4c
GhadeerHayek <ghadeerhayek2001@gmail.com>
2023-04-08 02:25:19 +0300

    feature: add login api

--- AppLogic/authentication.py
+++ AppLogic/authentication.py
@@ -64,4 +64,40 @@ def perform_signup():
 # TODO
 @auth_blueprint.route('/auth/login', methods=['POST'])
 def perform_login():
-    pass
+    username = request.form['username']
+    password = request.form['password']
+    # 1- verify the input and its format + escape it. 
+    # check inputs from being empty, ensure they're strings, and trim any leading or trailing spaces
+    check_username_condition = username and isinstance(username, str) and username.strip()
+    check_password_condition = password and isinstance(password, str) and password.strip()
+    if check_username_condition and check_password_condition:
+        # 2- perform queries on the database to locate the username and the password 
+        # NOTE: those are dummy inputs for test purposes only, they are replaced with db queries. 
+        # dummy_username = "Ghadeer"
+        # dummy_password = "ghadeer123"
+        cursor = mysql.connection.cursor()
+        query = "SELECT * from users where name='{0}' and password='{1}'".format(username, password)
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        # 3- after query is performed, then you should check whether you got a username and a correct password 
+        # apparently, the return type is tuple 
+        # 4- if match is found -> create token based on the user record on the database -> return token 
+        if len(result) > 0:
+            # a match is found
+            # generate token
+            token_payload ={
+                "id": result[0],
+                "name": result[1],
+                "email": result[2],
+                "password": result[3]
+            }
+            token = jwt.encode(token_payload,current_app.config['SECRET_KEY'], algorithm='HS256')
+            # prepare response 
+            response = make_response(jsonify(token_payload))
+            response.set_cookie('access_token', token, httponly=True)
+            return response
+        # 5- if no match, print an apropriate error message. 
+        else:
+            response_text = {'error': 'Invalid credentials, login failed'}
+            return jsonify(response_text)
