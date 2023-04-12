fb067b166abd0f3af5593b93f8f518865253e34f
GhadeerHayek <ghadeerhayek2001@gmail.com>
2023-04-12 11:53:34 +0300

    add file upload code to create test API

--- AppLogic/tests_operations.py
+++ AppLogic/tests_operations.py
@@ -3,9 +3,14 @@
 """
 
 from flask import Flask, Blueprint,jsonify
+from werkzeug.utils import secure_filename
 from database import mysql 
 # routes blueprint
 test_op_blueprint = Blueprint("test_op", __name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 """
     This API takes test data + authentication token as a parameter. Then, creates a test for the user in the database and returns its id.
@@ -14,9 +19,24 @@ test_op_blueprint = Blueprint("test_op", __name__)
 
 """
 @test_op_blueprint.route('/test/create', methods=['POST'])
-#TODO
 def create_user_test():
-    pass 
+    if 'file' not in request.files:
+        # file not in create test request, reject it 
+        pass
+    # receive file 
+    file = request.files['file']
+    # if the file has no name -> don't procceed.
+    if file.filename == '':
+        # no image is selected for uploading
+        pass 
+    # check if the file exists and its extension is allowd
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        # file is saved in the upload folder
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # perform processing 
+
+
 
 """
     This API is concerned with saving user adjustments on antibiotics labels and inhibition zone radius.
