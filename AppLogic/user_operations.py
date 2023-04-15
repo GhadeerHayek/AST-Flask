4b2571fff101cf02d97a5e5541de0b1c70a8e3bb
AyahElshawa <ayahs19302@gmail.com>
2023-04-15 21:04:47 +0200

    cleaned and refactored some code

--- AppLogic/user_operations.py
+++ AppLogic/user_operations.py
@@ -17,25 +17,27 @@ user_op_blueprint = Blueprint("user_op", __name__)
 """
 @user_op_blueprint.route('/user/tests', methods=['GET'])
 def get_user_tests():
+    # check if the access token exists
     if "access_token" not in request.cookies:
-        return jsonify({"status":"failure", "message": "missing token"})
+        return jsonify({"Status":"Failure", "Message": "Missing token"})
+    # get payload(user's data) from the token 
     token = request.cookies['access_token']
     payload = check_token_validity(token)
-    if payload:
-        user_id = payload["id"]
-        cursor = mysql.connection.cursor()
-        query = """SELECT * from tests where user_id = %(user_id)s"""
-        cursor.execute(query, {"user_id":user_id})
-        results = cursor.fetchall()
-        if results is None :
-            return jsonify({"tests_count": results_count, "data": []})   
-            # return jsonify({"status":"error", "message":"no tests found"})
-        else:
-            results_count = len(results)
-            # Convert result to list of dictionaries
-            columns = [column[0] for column in cursor.description]
-            result_dict = [dict(zip(columns, row)) for row in results]
-            return jsonify({"tests_count": results_count, "data": result_dict})                       
-    else:
-        return jsonify({"status":"error", "message":"token not valid"})
+    if payload is None:
+        return jsonify({"Status":"Error", "Message":"Token not valid"})
+    # get user id from the token 
+    user_id = payload["id"]
+    cursor = mysql.connection.cursor()
+    # get all user's test based on his/her id
+    query = """SELECT * from tests where user_id = %(user_id)s"""
+    cursor.execute(query, {"user_id":user_id})
+    results = cursor.fetchall()
+    if results is None :
+        return jsonify({"Tests_count": results_count, "data": []})   
+        # return jsonify({"status":"error", "message":"no tests found"})
+    results_count = len(results)
+    # Convert result to list of dictionaries
+    columns = [column[0] for column in cursor.description]
+    result_dict = [dict(zip(columns, row)) for row in results]
+    return jsonify({"tests_count": results_count, "data": result_dict})                       
     
