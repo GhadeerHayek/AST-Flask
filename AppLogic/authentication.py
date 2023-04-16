559a9e7217c53bf1354804eedf5f708dc2793c08
GhadeerHayek <ghadeerhayek2001@gmail.com>
2023-04-16 10:57:32 +0300

    refactor authentication.py, add comments and error messages

--- AppLogic/authentication.py
+++ AppLogic/authentication.py
@@ -1,10 +1,8 @@
 from config import Config
-from flask import current_app
-from flask import Flask, Blueprint, jsonify, request, make_response
+from flask import Flask, Blueprint, jsonify, request, make_response, current_app
 from database import mysql
 import jwt
-from AppLogic import token as token_logic
-from AppLogic import helper  
+from AppLogic import token as token_logic, helper
 
 # routes blueprint
 auth_blueprint = Blueprint("auth", __name__)
@@ -22,35 +20,40 @@ def perform_signup():
     username = request.form['username']
     password = request.form['password']
     email = request.form['email']
-    # check all inputs to add to database
+    # check all inputs then add to database
     if helper.validate_string(username) and helper.validate_string(password) and helper.validate_email(email):
+        # check if email is unique
         u_query = "SELECT email from users where email = %(user_email)s;"
         u_cursor = mysql.connection.cursor()
         u_cursor.execute(u_query, {"user_email": email})
         u_result = u_cursor.fetchone()
         u_cursor.close()
-        if u_result:
-            return jsonify({"status": "error", "message": "email is duplicated"})
-        else:
+        # email is unique,  insert
+        if u_result is None:
             i_query = "INSERT INTO users (name, email, password) VALUES (%(user_name)s, %(user_email)s, %(user_password)s);"
             i_cursor = mysql.connection.cursor()
             i_result = i_cursor.execute(
                 i_query, {"user_name": username, "user_email": email, "user_password": password})
             mysql.connection.commit()
             i_cursor.close()
+            # insertion success
             if i_result:
-                return jsonify({"status": "success", "message": "user is created, rows affected: {0}".format(str(i_result))})
+                return jsonify({"Status": "Success", "Message": "user is created, rows affected: {0}".format(str(i_result))})
+            # insertion failure
             else:
-                return jsonify({"status": "failure", "message": "failed to create"})
+                return jsonify({"Status": "Failure", "Message": "failed to create"})
+        # email is duplicated, no insertion
+        else:
+            return jsonify({"Status": "Failure", "Message": "email is duplicated"})
+    # input format not valid
     else:
-        # return error
-        return jsonify({"status": "error", "message": "invalid input"})
+        return jsonify({"Status": "Failure", "Message": "invalid input"})
 
 
 """ 
     This API performs login operation 
         - Input: email, password 
-        - Output: authentication token
+        - Output: authentication token in response cookies/headers
 """
 
 
@@ -59,10 +62,8 @@ def perform_login():
     # receive inputs
     username = request.form['username']
     password = request.form['password']
-    # check inputs from being empty, ensure they're strings, and trim any leading or trailing spaces
-    u = helper.validate_string(username)
-    p = helper.validate_string(password)
-    if u and p:
+    # check inputs
+    if helper.validate_string(username) and helper.validate_string(password):
         # check for credentials
         cursor = mysql.connection.cursor()
         query = " SELECT * from users where name =%(username)s and password= %(password)s "
@@ -70,17 +71,16 @@ def perform_login():
             query, {'username': username, "password": password})
         result = cursor.fetchone()
         cursor.close()
-        if result:
-            if len(result) > 0:
-                token = token_logic.generate_token(
-                    result, current_app.config['SECRET_KEY'])
-                # prepare response
-                response = make_response(
-                    jsonify({"status": "success", "message": "token is generated successfully."}))
-                response.set_cookie('access_token', token, httponly=True)
-                return response
-        # no match credentials
+        # no match, no user
+        if result is None:
+            return jsonify({"Status": "Failure", "Message": "Credentials not found, login failed"})
         else:
-            return jsonify({"status": "error", "message": "Invalid credentials, login failed"})
+            token = token_logic.generate_token(
+                result, current_app.config['SECRET_KEY'])
+            # prepare response
+            response = make_response(
+                jsonify({"Status": "Success", "Message": "token is generated successfully."}))
+            response.set_cookie('access_token', token, httponly=True)
+            return response
     else:
-        return jsonify({"status":"failure", "message":"invalid input", "u":u , "p":p})
+        return jsonify({"Status": "Failure", "Message": "invalid input"})
