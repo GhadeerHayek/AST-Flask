4fceece4e70990c1090d2ac8fabe599b1959d2c5
AyahElshawa <ayahs19302@gmail.com>
2023-05-10 21:05:50 +0200

    fixed the password hash

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ class Config:
     PROCESSED_FOLDER = 'static/processed/'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
     # NOTE: the host ip must be changed according to the device's current ip
-    MYSQL_HOST = '192.168.1.142'
-    MYSQL_USER = 'WSL_USER'
-    MYSQL_PASSWORD = ''
+    MYSQL_HOST = '192.168.1.104'
+    MYSQL_USER = 'WSL'
+    MYSQL_PASSWORD = 'ayah_unix'
     # this is the name of our database
-    MYSQL_DB = 'antibiotics_project'
+    MYSQL_DB = 'atb'
 
 
 """ 
@@ -39,7 +39,7 @@ class Config:
                 CROP_FOLDER = 'static/crops/'
                 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
                 # NOTE: the host ip must be changed according to the device's current ip
-                MYSQL_HOST = '192.168.1.109'
+                MYSQL_HOST = '192.168.1.104'
                 MYSQL_USER = 'WSL'
                 MYSQL_PASSWORD = 'ayah_unix'
                 # this is the name of our database
